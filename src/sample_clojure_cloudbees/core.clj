b35f7e658105c0167283bfd88381939fd41b5f22
Michael Neale <michael.neale@gmail.com>
2012-06-06 12:19:12 +1000

    annoying when docs are wrong

--- src/sample_clojure_cloudbees/core.clj
+++ src/sample_clojure_cloudbees/core.clj
@@ -2,9 +2,10 @@
   (:use compojure.core)  
   (:use ring.middleware.resource)  
   (:require 
-	  [compojure.route :as route]
-	  [compojure.handler :as handler]
-    [http.async.client :as http]))
+	  [compojure.route           :as route]
+	  [compojure.handler         :as handler]
+    [http.async.client         :as http]
+    [http.async.client.request :as request]))
 
 (defn open-url 
   "Open an async url connection"
@@ -19,7 +20,27 @@
   ))
 
 
+(defn open-callback 
+  "lets try this with a call back"
+  []
+  (with-open [client (http/create-client)] ; create client
+  (let [request (request/prepare-request :get "http://www.smh.com.au/") ; create request
+        status (promise)                ; status promise that will be delivered by callback
+        response (request/execute-request
+                  client request        ; execute *request*
+                  :status               ; status callback
+                  (fn [res st]          ; *res* is response map, same as one returned by *execute-request*
+                                        ; *st* is status map, as described above
+                    (deliver status st) ; deliver status promise
+                    [st :abort]))]      ; return status to be delivered to response map and abort further processing of response.
+    (println @status)))                 ; await status to be delivered and print it.
+
+  )
+
+
 (defn root-page []
+
+    (open-callback)
     (open-url "http://github.com/neotyk/http.async.client/")
   "OK")
 
