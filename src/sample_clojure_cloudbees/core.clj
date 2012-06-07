9510d9a80cbd45a356670999d3e8b4af55ecc924
Michael Neale <michael.neale@gmail.com>
2012-06-07 18:11:23 +1000

    more async goodness

--- src/sample_clojure_cloudbees/core.clj
+++ src/sample_clojure_cloudbees/core.clj
@@ -12,7 +12,7 @@
 
 
 (defn check-url 
-  "Open an async url connection - check if done"
+  "EXAMPLE Open an async url connection - check if done - using an agent"
   [url]  
   (print (str "checking:" url "\n"))  
   (print (str "Thread:" (Thread/currentThread)))
@@ -22,17 +22,41 @@
       (check-response resp))))
 
 
-
-(defn check-urls [urls]
+(defn check-urls 
+  "EXAMPLE shows how to use it with agents"
+  [urls]
   (let [agents (doall (map #(agent %) urls))]
     (doseq [agent agents] (send-off agent check-url))
     (apply await-for 5000 agents)
     (doall (map #(deref %) agents))))
 
 
+(defn ping-url 
+  "Open an async url connection - check if done"
+  [client url]  
+    (http/GET client url :timeout 5000)) 
+
+(defn ping-urls [urls]
+  (with-open [client (http/create-client)] ; Create client
+    (println "starting")
+    (let [requests (doall (map #(ping-url client %) urls))]
+      (println "dispatched.. now resting")
+      (Thread/sleep 5000)
+      (doall (map #(check-response %) requests)))))
+
+
+
+
+
+
+
+
+
+
 (defn root-page []
-    (prn (check-urls 
-      '("http://lethain.com" "http://willarson.com" "http://www.smh.com.aux")
+    (prn (ping-urls 
+      '(
+        "http://lethain.com" "http://willarson.com" "http://www.smh.com.aux")
       ))  
     "OK")
 
